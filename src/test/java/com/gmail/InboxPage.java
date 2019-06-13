d59ab0c1833c388c696ba617e86135ba44fcc9f6
Пустовая Александра <Shurenochek_zi@mail.ru>
2019-06-13 16:43:56 +0300

    вместо чекбокса "Выделить все" используется выпадающий список и после выпадающего списка выбрать все

--- src/test/java/com/gmail/InboxPage.java
+++ src/test/java/com/gmail/InboxPage.java
@@ -8,31 +8,38 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class InboxPage extends WebdriverInitial {
-    private WebDriver driver;
-    public InboxPage(WebDriver driver)
+    private final WebDriver driver;
+    public InboxPage( final WebDriver driver)
     {
         this.driver = driver;
         PageFactory.initElements(driver,this);
     }
-    @FindBy (xpath = "/html/body/div[7]/div[3]/div/div[2]/div[1]/div[2]/div/div/div/div/div[1]/div/div[1]/div[1]/div/div/div[1]/div/div[1]/span")
-    private WebElement selectAllCheckbox;
-    @FindBy (css= ".nX > div:nth-child(1)")
-    private WebElement deleteAll;
-    @FindBy (xpath = "/html/body/div[7]/div[3]/div/div[2]/div[1]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div/div[6]/div/div[1]/div[1]/div[2]/div/div/div")
+    //кнопка выпадающего списка
+    @FindBy (css  = "[class='G-asx T-I-J3 J-J5-Ji']:nth-child(2)")
+    private WebElement arrowDown;
+    // выбрать все
+    @FindBy(css = "[selector='all']")
+    private WebElement selectAll;
+    // удалить все
+   @FindBy(css = "[class='T-I J-J5-Ji nX T-I-ax7 T-I-Js-Gs mA']")
+   private WebElement deleteAll;
+    // сообщение о пустом ящике
+    @FindBy(css = "#\\:28 > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)")
     private WebElement messageEmpty;
     public void setSelectAllCheckbox()
     {
-        WebDriverWait wait = new WebDriverWait(driver, 30);
-        wait.until(ExpectedConditions.visibilityOf(selectAllCheckbox));
-        if(!selectAllCheckbox.isSelected())
-        {
-            selectAllCheckbox.click();
-        }
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        //ожидание пока кнопка выпадающего списка станет кликабельной
+        wait.until(ExpectedConditions.elementToBeClickable(arrowDown));
+        arrowDown.click();
+        //ожидание пока кнопка "Выбрать все" станет кликабельной
+        wait.until(ExpectedConditions.elementToBeClickable(selectAll));
+        selectAll.click();
     }
     // нажать на кнопку "Удалить"
     public InboxPage clickDeleteAll()
     {
-        WebDriverWait wait = new WebDriverWait(driver, 30);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOf(deleteAll));
         deleteAll.click();
         return this;
